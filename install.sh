cf61adca52ca4abfe0d88ea12af72835ef42fb1d
Gamerzhacker😎 <bestservergamer@gmail.com>
2025-04-01 19:46:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if ! command -v python3 &> /dev/null; then
 fi
 
 # Verify the key
-CORRECT_KEY="hk-i9"
+CORRECT_KEY="hk-i2"
 read -p "Enter the setup key to proceed: " USER_KEY
 
 if [[ "$USER_KEY" != "$CORRECT_KEY" ]]; then
@@ -31,7 +31,7 @@ fi
 echo "Key verified successfully. Proceeding with setup..."
 
 # Clone the repository
-REPO_URL="https://github.com/Gamerzhacker/hk-i9.git"
+REPO_URL="https://github.com/Gamerzhacker/hk-i10.git"
 echo "Cloning the repository..."
 git clone "$REPO_URL" || { echo "Failed to clone repository."; exit 1; }
 
